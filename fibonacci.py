ee442241712917c7b2067f38939d1d48238fa192
N8Brooks <natebrooks004@gmail.com>
2019-10-20 13:21:09 -0500

    Create fibonacci.py
    Methods of study

---
+++ fibonacci.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 20 13:15:21 2019
+
+@author: DSU
+"""
+
+# dynamic implementation
+def FibLoop(x: int) -> int:
+    pass
+
+# brute force recursive
+def FibRecur(x: int) -> int:
+    pass
+
+# recursive with memoization
+def FibRecurDP(x: int) -> int:
+    pass
+
+# helper method for FibMatrix
+def MatrixPower(x: int) -> int:
+    pass
+
+# matrix multiplication method
+def FibMatrix(x: int) -> int:
+    pass
